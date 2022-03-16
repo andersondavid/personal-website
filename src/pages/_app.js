9db64d32c9be160a3ae487e15c20fb634798aa90
Anderson David <andersondaviddev@gmail.com>
2022-03-16 00:02:31 -0300

    set _document.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,14 @@
+import Head from 'next/head'
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+	return (
+		<>
+			<Head>
+				<title>My new cool app</title>
+			</Head>
+			<Component {...pageProps} />
+		</>
+	);
 }
 
-export default MyApp
+export default MyApp;
