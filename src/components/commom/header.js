92347e0649e04989e4fa14f5bb3f1bc75b567446
Anderson David <andersondaviddev@gmail.com>
2022-03-21 00:23:12 -0300

    header and sidebar finished

--- src/components/commom/header.js
+++ src/components/commom/header.js
@@ -1,62 +1,113 @@
 import Image from 'next/image'
 import styled from 'styled-components'
+import { useState } from 'react'
 
 import logo from '../../../public/images/david.svg'
+import menuBtn from '../../../public/images/icons/menu.svg'
 
 const HeaderTag = styled.header`
 	width: 100%;
 	display: flex;
 	justify-content: space-between;
 	z-index: 1000;
+	position: relative;
 	grid-column-start: 1;
 	grid-column-end: 2;
 	grid-row-start: 1;
 	grid-row-end: 1;
+	&::before {
+		content: '';
+		height: 1px;
+		width: 100%;
+		background: ${props => props.theme.lines};
+		position: absolute;
+		bottom: 0;
+	}
+	@media (max-width: 500px) {
+		grid-column-start: 1;
+		grid-column-end: 1;
+		grid-row-start: 1;
+		grid-row-end: 1;
+	}
 `
 
 const LogoContainer = styled.div`
 	padding: 16.5px;
 `
 
-const LogoIcon = styled(Image)`
+const HeaderIcons = styled(Image)`
 	height: 100%;
+`
+
+const MenuBtnContainer = styled.div`
+	padding: 16.5px;
+	@media (min-width: 500px) {
+		display: none;
+	}
+`
+
+const NavBar = styled.nav`
+	transition: 0.3s;
+	@media (max-width: 500px) {
+		position: absolute;
+		top: ${props => props.linksOpen ? '64px' : '-220px'};
+		right: 0;
 `
 
 const SocialLinksList = styled.ul`
 	list-style: none;
+	display: inline-block;
+	background: ${props => props.theme.bg};
+	@media (max-width: 500px) {
+		border: solid 1px ${props => props.theme.lines};
+		background: ${props => props.theme.bg};
+		display: block;
+	}
 	li {
-		display: inline-block;
 		height: 65px;
-		line-height: 65px;
-		padding: 0 15px;
-		color: #ffffff;
 		transition: 0.3s;
 		text-transform: uppercase;
-		&:hover {
-			background-color: rgb(255, 255, 255, 0.1);
-		}
+		font-family: 'Raleway',sans-serif;
+		display: inherit;
 	}
-`
+	a {
+		color: ${props => props.theme.textPrimary};
+		text-decoration: none;
+		line-height: 65px;
+		padding: 15px;
+		margin-right: 15px;
+	}
+	li:hover {
+		background-color: ${props => props.theme.bgLight};
+	}
+	`
 
 export default function Header() {
+	const [linksOpen, setLinksOpen] = useState(false)
+
 	return (
 		<HeaderTag>
 			<LogoContainer>
-				<LogoIcon src={logo} height={'32px'} width={'32px'} />
+				<HeaderIcons src={logo} height='32px' width='32px' />
 			</LogoContainer>
-			<nav>
+			<MenuBtnContainer onClick={() => setLinksOpen(!linksOpen)} >
+				<HeaderIcons src={menuBtn} height='32px' width='32px' />
+			</MenuBtnContainer>
+			<NavBar linksOpen={linksOpen}>
 				<SocialLinksList>
-					<a href="">
-						<li>Instagram</li>
-					</a>
-					<a href="">
-						<li>Pinterest</li>
-					</a>
-					<a href="">
-						<li>LinkedIN</li>
-					</a>
+					<li>
+						<a href='//github.com/andersondavid'>github</a>
+					</li>
+					
+					<li>
+						<a href='//www.linkedin.com/in/anderson-david-349b18187'>linkedin</a>
+					</li>
+					
+					<li>
+						<a href='//pinterest.com/ndersondavid/'>pinterest</a>
+					</li>
 				</SocialLinksList>
-			</nav>
+			</NavBar>
 		</HeaderTag>
 	)
 }
--- src/components/commom/sidebar.js
+++ src/components/commom/sidebar.js
@@ -1,16 +1,116 @@
 import styled from 'styled-components'
+import Image from 'next/image'
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+// icons
+import home from '../../../public/images/icons/home.svg'
+import person from '../../../public/images/icons/person.svg'
+import code from '../../../public/images/icons/code.svg'
+import github from '../../../public/images/icons/github.svg'
 
 const Aside = styled.aside`
 	grid-column-start: 2;
 	grid-column-end: 3;
 	grid-row-start: 1;
 	grid-row-end: 3;
+	position: relative;
+	&::after {
+		top: 0;
+		left: 0;
+		position: absolute;
+		height: 100%;
+		width: 1px;
+		content: '';
+		background: ${props => props.theme.lines};
+	}
+	@media (max-width: 500px) {
+		grid-column-start: 1;
+		grid-column-end: 1;
+		grid-row-start: 3;
+		grid-row-end: 3;
+		&::after {
+			height: 1px;
+			width: 100%;
+		}
+	}
+`
+
+const PagesLinksList = styled.ul`
+	list-style: none;
+	display: flex;
+	flex-direction: column;
+	height: 100%;
+	justify-content: center;
+	align-items: center;
+	@media (max-width: 500px){
+		flex-direction: row;
+		justify-content: space-evenly;
+	}
+`
+
+const PagesLinksItems = styled.li`
+	cursor: pointer;
+	margin: 15px 0;
+	padding: 10px;
+	border-radius: 50%;
+	height: 52px;
+	width: 52px;
+	transition: 0.3s;
+	background: ${props => props.isActive ? props.theme.bgLight : 'initial'};
+	a {
+		pointer-events: ${props => props.isActive ? 'none' : 'auto'};
+	}
+	&:hover {
+		background: ${props => props.theme.bgLight};
+	}
+	@media (max-width: 500px){
+		height: 42px;
+		width: 42px;
+	}
 `
 
 export default function SideBar() {
+	const router = useRouter()
+	
+	const isActive = (href) => href === router.pathname
+
 	return (
 		<Aside>
-			<h2>asdasdas</h2>
+			<PagesLinksList>
+
+				<PagesLinksItems isActive={isActive('/')}>
+					<Link href='/' >
+						<a>
+							<Image src={home} />
+						</a>
+					</Link>
+				</PagesLinksItems>
+
+				<PagesLinksItems isActive={isActive('/github')}>
+					<Link href='/github'>
+						<a disable='true' >
+							<Image src={github} />
+						</a>
+					</Link>
+				</PagesLinksItems>
+
+				<PagesLinksItems isActive={isActive('/aboutme')}>
+					<Link href='/aboutme'>
+						<a>
+							<Image src={person} />
+						</a>	
+					</Link>
+				</PagesLinksItems>
+
+				<PagesLinksItems isActive={isActive('/thissite')}>
+					<Link href='/thissite'>
+						<a>
+							<Image src={code} />
+						</a>
+					</Link>
+				</PagesLinksItems>
+
+			</PagesLinksList>
 		</Aside>
 	)
 }
